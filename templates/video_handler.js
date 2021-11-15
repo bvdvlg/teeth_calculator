08d59127bf4bb98a9d9384661c78892e9a56e765
bvdvlg <bakumov.vd@phystech.edu>
2021-11-15 18:18:52 +0300

    s

--- templates/video_handler.js
+++ templates/video_handler.js
@@ -2,7 +2,19 @@ class Camera {
     constraints
     onFrame
 
-    constructor(constraints, onFrame, loadmetadata) {
+    constructor(constraints, onFrame) {
+        this.constraints = constraints;
+        this.onFrame = onFrame;
+        try {
+            this.get_media_device(constraints).then((stream) => {this.startProcessing(stream)});
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    restart(constraints, onFrame) {
+        this.clear();
         this.constraints = constraints;
         this.onFrame = onFrame;
         try {
@@ -31,11 +43,21 @@ class Camera {
     }
 
     threatFrame(video) {
-        this.onFrame().then(() => {this.requestFrame(video)});
+        if (!video.paused)
+            this.onFrame().then(() => {this.requestFrame(video)});
     }
 
     async get_media_device(constraints) {
         navigator.mediaDevices && navigator.mediaDevices.getUserMedia || alert("No navigator.mediaDevices.getUserMedia exists.");
         return navigator.mediaDevices.getUserMedia(constraints);
+    }
+
+    clear () {
+        if (window.stream) {
+            window.stream.getTracks().forEach(function (track) {
+                track.stop();
+                console.log("Abort stream")
+            });
+        }
     }
 }

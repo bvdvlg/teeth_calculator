2e6cf9a4bd3a8d733d7d68cf70cd911aef127875
bvdvlg <bakumov.vd@phystech.edu>
2021-11-18 19:40:25 +0300

    s

---
+++ mainapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from mainapp import views
+from django.shortcuts import redirect
+
+urlpatterns = [
+    path('camera/', views.camera, name="camera"),
+    path('about/', views.about, name="about"),
+    path('main/', views.main, name="main"),
+    path('results/', views.results, name="results")]
